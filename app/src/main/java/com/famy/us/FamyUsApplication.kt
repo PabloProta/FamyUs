41081aa2ea8149729601181d868df7675e9b4237
Pablo Antonini Prota-PPROTA <pprota@motorola.com>
2023-04-09 14:01:39 -0300

    🎉Initial commit!

---
+++ app/src/main/java/com/famy/us/FamyUsApplication.kt
@@ -0,0 +1,6 @@
+package com.famy.us
+
+import android.app.Application
+
+class FamyUsApplication : Application() {
+}
---
+++ app/src/main/java/com/famy/us/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.famy.us
+
+import FamyUsTheme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FamyUsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("CHARAMMM")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FamyUsTheme {
+        Greeting("CHARAMMMM")
+    }
+}
