bb0b696c9cca40304f8105393cbab2a277ff5898
Pablo Antonini Prota-PPROTA <pprota@motorola.com>
2024-04-10 19:12:01 -0300

    ⛲️ Insert new fonts
    Inserted the new fonts based on
    new design spec.

---
+++ core/ui/src/main/java/com/famy/us/core/ui/Fonts.kt
@@ -0,0 +1,29 @@
+package com.famy.us.core.ui
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+val familjenGrotesk = FontFamily(
+    Font(R.font.familjen_grotesk_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.familjen_grotesk_bold, FontWeight.Bold),
+    Font(R.font.familjen_grotesk_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.familjen_grotesk_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.familjen_grotesk_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.familjen_grotesk_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.familjen_grotesk_regular, FontWeight.Normal),
+    Font(R.font.familjen_grotesk_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+)
+
+val inter = FontFamily(
+    Font(R.font.inter_black,FontWeight.Black, FontStyle.Normal),
+    Font(R.font.inter_bold,FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.inter_extra_bold,FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.inter_extra_light,FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.inter_light,FontWeight.Light, FontStyle.Normal),
+    Font(R.font.inter_medium,FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.inter_regular,FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.inter_semi_bold,FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.inter_thin,FontWeight.Thin, FontStyle.Normal),
+)
---
+++ core/ui/src/main/java/com/famy/us/core/ui/Typo.kt
@@ -0,0 +1,136 @@
+package com.famy.us.core.ui
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// HEADLINE
+val H7 = TextStyle(
+    fontFamily = familjenGrotesk,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 16.sp,
+    lineHeight = 20.sp,
+)
+
+val H6 = TextStyle(
+    fontFamily = familjenGrotesk,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 20.sp,
+    lineHeight = 24.sp,
+)
+
+val H5 = TextStyle(
+    fontFamily = familjenGrotesk,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 24.sp,
+    lineHeight = 32.sp,
+)
+
+val H4 = TextStyle(
+    fontFamily = familjenGrotesk,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 32.sp,
+    lineHeight = 40.sp,
+)
+
+val H3 = TextStyle(
+    fontFamily = familjenGrotesk,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 40.sp,
+    lineHeight = 48.sp,
+)
+
+val H2 = TextStyle(
+    fontFamily = familjenGrotesk,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 48.sp,
+    lineHeight = 56.sp,
+)
+
+val H1 = TextStyle(
+    fontFamily = familjenGrotesk,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 56.sp,
+    lineHeight = 64.sp,
+)
+
+// SUBTITLE
+
+val Caption = TextStyle(
+    fontFamily = inter,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+    lineHeight = 16.sp,
+)
+
+val Subtitle = TextStyle(
+    fontFamily = inter,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 16.sp,
+    lineHeight = 20.sp,
+)
+
+// BODY
+val BodySmallMedium = TextStyle(
+    fontFamily = inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp,
+    lineHeight = 18.sp,
+)
+
+val BodySmallRegular = TextStyle(
+    fontFamily = inter,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+    lineHeight = 18.sp,
+)
+
+val BodyMediumMedium = TextStyle(
+    fontFamily = inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp,
+    lineHeight = 20.sp,
+)
+
+val BodyMediumRegular = TextStyle(
+    fontFamily = inter,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp,
+    lineHeight = 20.sp,
+)
+
+val BodyLargeMedium = TextStyle(
+    fontFamily = inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 18.sp,
+    lineHeight = 22.sp,
+)
+
+val BodyLargeRegular = TextStyle(
+    fontFamily = inter,
+    fontWeight = FontWeight.Normal,
+    fontSize = 18.sp,
+    lineHeight = 22.sp,
+)
+
+// Button
+val ButtonSmall = TextStyle(
+    fontFamily = familjenGrotesk,
+    fontWeight = FontWeight.Bold,
+    fontSize = 14.sp,
+    lineHeight = 24.sp,
+)
+
+val ButtonMedium = TextStyle(
+    fontFamily = familjenGrotesk,
+    fontWeight = FontWeight.Bold,
+    fontSize = 16.sp,
+    lineHeight = 24.sp,
+)
+
+val ButtonLarge = TextStyle(
+    fontFamily = familjenGrotesk,
+    fontWeight = FontWeight.Bold,
+    fontSize = 18.sp,
+    lineHeight = 24.sp,
+)
