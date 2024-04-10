67548d8563225811bd8865ac57a67ecbefe3477d
Pablo Antonini Prota-PPROTA <pprota@motorola.com>
2024-04-10 18:16:26 -0300

    🌟 Insert new colors
    Inserted the new colors based on
    new design spec.

---
+++ core/ui/src/main/java/com/famy/us/core/ui/Colors.kt
@@ -0,0 +1,42 @@
+package com.famy.us.core.ui
+
+import androidx.compose.ui.graphics.Color
+
+// PRIMARY COLOR
+val primary_50 = Color(0XFFF9F0FF)
+val primary_100 = Color(0XFFEAD4FF)
+val primary_200 = Color(0XFFDAB8FF)
+val primary_300 = Color(0XFFC89DFF)
+val primary_400 = Color(0XFFB384F7)
+val primary_500 = Color(0XFF9D6DE8)
+val primary_600 = Color(0XFF8758D2)
+val primary_700 = Color(0XFF7046B7)
+val primary_800 = Color(0XFF5A3698)
+val primary_900 = Color(0XFF452974)
+val primary_main = Color(0XFFA371EF)
+
+// SECONDARY COLOR
+val secondary_50 = Color(0XFFFEF2E3)
+val secondary_100 = Color(0XFFF9D8AE)
+val secondary_200 = Color(0XFFEEBE7C)
+val secondary_300 = Color(0XFFDFA64F)
+val secondary_400 = Color(0XFFCC9024)
+val secondary_500 = Color(0XFFB67A00)
+val secondary_600 = Color(0XFF9F6600)
+val secondary_700 = Color(0XFF865300)
+val secondary_800 = Color(0XFF6D4200)
+val secondary_900 = Color(0XFF543200)
+val secondary_main = Color(0XFFE09D21)
+
+// TERNARY COLOR
+val tertiary_50 = Color(0XFFF4F3F6)
+val tertiary_100 = Color(0XFFDEDBE5)
+val tertiary_200 = Color(0XFFC8C4D4)
+val tertiary_300 = Color(0XFFB2AEC2)
+val tertiary_400 = Color(0XFF9D98AF)
+val tertiary_500 = Color(0XFF88839B)
+val tertiary_600 = Color(0XFF746F86)
+val tertiary_700 = Color(0XFF605C71)
+val tertiary_800 = Color(0XFF4D495B)
+val tertiary_900 = Color(0XFF3A3746)
+val tertiary_main = Color(0XFF120D21)
