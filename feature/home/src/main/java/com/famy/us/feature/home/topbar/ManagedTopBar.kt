0290e756b4730cae70fcb254813fbb39181aaf68
Pablo Antonini Prota-PPROTA <pprota@motorola.com>
2024-04-24 18:17:27 -0300

    Build the Managed top bar

--- feature/home/src/main/java/com/famy/us/feature/home/topbar/ManagedTopBar.kt
+++ feature/home/src/main/java/com/famy/us/feature/home/topbar/ManagedTopBar.kt
@@ -1,18 +1,24 @@
 package com.famy.us.feature.home.topbar
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,8 +28,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
+import com.famy.us.core.ui.BodySmallMedium
 import com.famy.us.core.ui.BodySmallRegular
 import com.famy.us.core.ui.ButtonLarge
 import com.famy.us.core.ui.primary_main
@@ -31,12 +36,11 @@ import com.famy.us.core.ui.tertiary_50
 import com.famy.us.core.ui.tertiary_main
 import com.famy.us.home.R
 
-@Suppress("MagicNumber")
 @Composable
-fun AdminTopBar(
+fun ManagedTopBar(
     familyName: String,
     memberName: String,
-    role: String,
+    score: Int,
 ) {
     Surface(
         shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp),
@@ -44,41 +48,71 @@ fun AdminTopBar(
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
                 .safeDrawingPadding()
+                .fillMaxWidth()
                 .padding(
                     vertical = 9.dp,
                     horizontal = 24.dp,
                 ),
             verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
         ) {
             ImagePlaceHolder()
             Spacer(modifier = Modifier.size(12.dp))
             Box(
-                modifier = Modifier.weight(3f),
+                modifier = Modifier
+                    .weight(1f),
             ) {
                 TextsInputs(
                     memberName = memberName,
                     familyName = familyName,
-                    role = role,
                 )
             }
-            Spacer(modifier = Modifier.size(8.dp))
-            NotificationContainer()
+            Box(
+                modifier = Modifier
+                    .weight(1f),
+                contentAlignment = Alignment.CenterEnd,
+            ) {
+                NotificationContainer(
+                    score = score,
+                )
+            }
         }
     }
 }
 
-@Suppress("MagicNumber")
+private const val NumberLimit = 10000
+private const val Thousand = 1000
+private const val DotCount = 3
+
+fun formatIntsWithDecimal(input: Int): String {
+    var number = if (input >= NumberLimit) input / Thousand else input
+    val inputString = number.toString()
+    val length = inputString.length
+
+    val result = StringBuilder()
+
+    var i = length - 1
+    var count = 0
+    while (i >= 0) {
+        result.append(inputString[i])
+        count++
+        if (count % DotCount == 0 && i != 0) {
+            result.append('.')
+        }
+        i--
+    }
+
+    val appendK = if (input > NumberLimit) "K" else ""
+    return result.reverse().toString() + appendK
+}
+
 @Composable
 private fun TextsInputs(
     memberName: String,
     familyName: String,
-    role: String,
 ) {
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-    ) {
+    Column {
         Text(
             text = "Hey, $memberName \uD83D\uDC4B\uD83C\uDFFC",
             maxLines = 1,
@@ -87,48 +121,34 @@ private fun TextsInputs(
             color = tertiary_50,
         )
         Spacer(modifier = Modifier.size(4.dp))
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxWidth(),
-        ) {
-            val familyRef = createRef()
-            val roleRef = createRef()
-            Text(
-                modifier = Modifier
-                    .constrainAs(familyRef) {
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-                    },
-                text = "Família ${familyName.take(20)}",
-                softWrap = false,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                style = BodySmallRegular,
-                color = primary_main,
-            )
-            Text(
-                modifier = Modifier
-                    .constrainAs(roleRef) {
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-                        end.linkTo(parent.end)
-                    },
-                text = " • $role",
-                maxLines = 1,
-                style = BodySmallRegular,
-                color = primary_main,
-            )
-            createHorizontalChain(familyRef, roleRef, chainStyle = ChainStyle.Packed(0f))
-        }
+        Text(
+            text = "Família $familyName",
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            style = BodySmallRegular,
+            color = primary_main,
+        )
     }
 }
 
 @Composable
-private fun NotificationContainer() {
-    Surface(
-        shape = RoundedCornerShape(12.dp),
-        color = Color.White.copy(alpha = 0.04f),
+private fun NotificationContainer(score: Int) {
+    Row(
+        modifier = Modifier
+            .height(IntrinsicSize.Min)
+            .background(
+                color = Color.White.copy(alpha = 0.04f),
+                shape = RoundedCornerShape(12.dp),
+            )
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
+        ScoreContainer(score = score)
+        Spacer(modifier = Modifier.size(8.dp))
+        VerticalDivider(
+            thickness = 1.dp,
+            color = Color.White.copy(alpha = 0.04f),
+        )
         Image(
             modifier = Modifier
                 .padding(8.dp),
@@ -138,6 +158,24 @@ private fun NotificationContainer() {
             contentDescription = null,
         )
     }
+}
+
+@Composable
+private fun ScoreContainer(
+    score: Int,
+) {
+    Image(
+        painter = painterResource(id = R.drawable.img_coin),
+        contentDescription = null,
+    )
+    Spacer(modifier = Modifier.size(4.dp))
+    Text(
+        modifier = Modifier
+            .width(42.dp),
+        text = formatIntsWithDecimal(score),
+        style = BodySmallMedium,
+        color = tertiary_50,
+    )
 }
 
 @Composable
@@ -155,10 +193,10 @@ private fun ImagePlaceHolder() {
 
 @Preview
 @Composable
-internal fun AdminTopBarPreview() {
-    AdminTopBar(
+internal fun ManagedTopBarPreview() {
+    ManagedTopBar(
         familyName = "Prota",
         memberName = "Pablo",
-        role = "Root",
+        score = 9500,
     )
 }
